b7eb9b141adf970a73a82f71c3c0209ac358bfe6
Conner Starkel <csmediaseattle@gmail.com>
2025-01-04 14:23:53 -0800

    feat: Add json output file

---
+++ DivoomDoneQuick/CollectedData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivoomDoneQuick
+{
+    public class CollectedData
+    {
+        public string eventName { get; set; }
+        public string donations { get; set; }
+        public string scheduleOne { get; set; }
+        public string scheduleTwo { get; set; }
+        public string scheduleThree { get; set; }
+    }
+}
